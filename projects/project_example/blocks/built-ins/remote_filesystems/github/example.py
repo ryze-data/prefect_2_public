45499cf6b4b64c9715908819ddcbb7be30da4d24
Matt Jeffs <m.s.jeffs50@live.com>
2023-05-16 14:19:11 -0600

    Update example.py

--- projects/project_example/blocks/built-ins/remote_filesystems/github/example.py
+++ projects/project_example/blocks/built-ins/remote_filesystems/github/example.py
@@ -1,9 +1,9 @@
 from prefect.filesystems import GitHub
 
 block = GitHub(
-    repository="https://github.com/my-repo/",
-    access_token=<my_access_token> # only required for private repos
+    repository="https://github.com/ryze-data/prefect_2_public/"",
+    # access_token=<my_access_token> # only required for private repos
 )
-block.get_directory("folder-in-repo") # specify a subfolder of repo
-block.save("dev")
+block.get_directory("projects/project_example"") # specify a subfolder of repo
+block.save("project_example")
 
