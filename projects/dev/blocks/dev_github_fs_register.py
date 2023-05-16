faae258de19c14cba82407b61c2aa1b42838aba8
Matt Jeffs <m.s.jeffs50@live.com>
2023-05-16 14:18:05 -0600

    Create dev_github_fs_register.py

---
+++ projects/dev/blocks/dev_github_fs_register.py
@@ -0,0 +1,8 @@
+from prefect.filesystems import GitHub
+
+block = GitHub(
+    repository="https://github.com/ryze-data/prefect_2_public/"
+     # only required for private repos
+)
+block.get_directory("projects/dev") # specify a subfolder of repo
+block.save("dev")
