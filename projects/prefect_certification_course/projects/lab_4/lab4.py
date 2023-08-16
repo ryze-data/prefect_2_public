22cf21221b85ae955adbc468fcf8a2153cac35ca
Matt Jeffs <mjeffs@gopfs.com>
2023-08-16 14:24:42 -0600

    upload deployment flow test

--- projects/prefect_certification_course/projects/lab_4/lab4.py
+++ projects/prefect_certification_course/projects/lab_4/lab4.py
@@ -25,22 +25,23 @@ def save_weather(temp: float):
     with open("weather.csv", "w+") as w:
         w.write(str(temp))
     return "Succefully wrote temp"
-@task(retries=2, retry_delay_seconds=2)
-def windspeed(lat: float, lon: float):
-    base_url = "https://api.open-meteo.com/v1/forecast/"
-    wind = httpx.get(
-        base_url,
-        params = dict(latitude=lat, longitude=lon, hourly="temperature_2m"),
-    )
-    wind_speed = float(wind.json()["hourly"]["windspeed_10m"][0])
-    print(f"Current wind speed C: {wind_speed}")
-    return wind_speed
+# @task(retries=2, retry_delay_seconds=2)
+# def windspeed(lat: float, lon: float):
+#     base_url = "https://api.open-meteo.com/v1/forecast/"
+#     wind = httpx.get(
+#         base_url,
+#         params = dict(latitude=lat, longitude=lon, hourly="temperature_2m"),
+#     )
+#     wind_speed = float(wind.json()["hourly"]["windspeed_10m"][0])
+    # print(f"Current wind speed C: {wind_speed}")
+    # return wind_speed
     
 @flow(retries=3)
-def pipeline(lat: float, lon: float):
+def sample_pipeline(lat: float, lon: float):
     temp = fetch_weather(lat, lon)
     result = save_weather(temp)
-    curr_wind = windspeed(lat, lon)
-    print(f"Current temp C: {temp} degrees and windspeed is {curr_wind}")
+    # curr_wind = windspeed(lat, lon)
+    print(f"Current temp C: {temp} degrees ")
+    # and windspeed is {curr_wind}")
 if __name__ == "__main__":
-    fetch_weather(38.9, -77.0)
+    sample_pipeline(38.9, -77.0)
